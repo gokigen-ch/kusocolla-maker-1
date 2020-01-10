d85ac65fa5631f7eedfe083c4b18387433a5f29a
Daisuke Saito <dsaito85a@gmail.com>
2020-01-10 16:40:37 +0900

    feat: 画像アップロード前のプレビューを追加

--- public/js/app.js
+++ public/js/app.js
@@ -37006,6 +37006,8 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./updateImageDisplay */ "./resources/js/updateImageDisplay.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
@@ -37050,6 +37052,73 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/updateImageDisplay.js":
+/*!********************************************!*\
+  !*** ./resources/js/updateImageDisplay.js ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var form = document.querySelector('#image-form');
+var label = form.querySelector('label');
+var input = label.querySelector('input');
+var preview = form.querySelector('.preview');
+var button = form.querySelector('button');
+input.addEventListener('change', updateImageDisplay);
+
+function updateImageDisplay() {
+  // 前回アップロードした情報を削除
+  while (preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  var curFiles = input.files; // 画像ファイルがアップロードされてないとき
+
+  if (curFiles.length === 0) {
+    // labelを有効色にする
+    label.classList.add('btn-primary');
+    label.classList.remove('btn-secondary'); // buttonを無効色にする
+
+    button.classList.remove('btn-primary');
+    button.classList.add('btn-secondary'); // 画像ファイルがアップロードされたとき
+  } else {
+    // labelを無効色にする
+    label.classList.add('btn-secondary');
+    label.classList.remove('btn-primary'); // buttonを有効色にする
+
+    button.classList.add('btn-primary');
+    button.classList.remove('btn-secondary');
+    var list = document.createElement('ol');
+    preview.appendChild(list); // 複数ファイル選択はできない
+
+    if (validFileType(curFiles[0])) {
+      var image = document.createElement('img');
+      image.src = window.URL.createObjectURL(curFiles[0]);
+      image.style.maxWidth = '50%';
+      preview.appendChild(image);
+    } else {
+      var para = document.createElement('p');
+      para.textContent = 'ファイル名 ' + curFiles[0].name + ' はファイル形式が有効ではありません。選択し直してください。';
+      preview.appendChild(para);
+    }
+  }
+}
+
+var fileTypes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/jpg' // 特定のIEでたまにこの値になるらしい
+];
+
+function validFileType(file) {
+  for (var i = 0; i < fileTypes.length; i++) {
+    if (file.type === fileTypes[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 /***/ }),
 
