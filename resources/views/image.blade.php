d13b7d124d7fa74e901ce4f3f658bd72da98dcd4
yamada <eri.genkidama@gmail.com>
2020-01-14 22:19:15 +0900

    クソコラを動いた

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -20,6 +20,7 @@
                 @if ($image != null)
                 <p>合成した画像</p>
                 <div class="kusocolla">
+                    <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-01.jpg" alt="">
                     <div class="kusocolla__face face-01">
                         @for($i =0; $i<$image->face_num; $i++)
                             <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
@@ -35,8 +36,22 @@
                             <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
                         @endfor
                     </div>
+                    <div class="kusocolla__face face-05 move02">
+                        @for($i =0; $i<$image->face_num; $i++)
+                            <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                        @endfor
+                    </div>
+                    <div class="kusocolla__img-move move01">
+                        <div class="kusocolla__img__inner">
+                            <div class="kusocolla__face face-04">
+                                @for($i =0; $i<$image->face_num; $i++)
+                                    <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                                @endfor
+                            </div>
+                            <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-02.png" alt="">
+                        </div>
+                    </div>
                 </div>
-                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/add.{{$image->getExtension()}}" alt="image" style="max-width:100%">
                 <p>顔だけ切り出した画像</p>
                 @for($i =0; $i<$image->face_num; $i++)
                     <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
