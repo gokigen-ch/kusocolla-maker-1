1bf40be038bfb8064c202c692c12ed1cccc924a6
yamada <eri.genkidama@gmail.com>
2020-01-31 17:02:08 +0900

    クソコラ02の顔を配置

--- resources/views/kusocolas/2.blade.php
+++ resources/views/kusocolas/2.blade.php
@@ -1,11 +1,24 @@
     <div class="kusocolla02">
-        <div class="kusocolla02__img-move move01">
-            <div class="kusocolla02__img__inner">
-                <div class="kusocolla02__face face-01">
-                    @for($i =0; $i<$image->face_num; $i++)
-                        <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-                    @endfor
-                </div>
+        <div class="kusocolla02__img__inner">
+            <div class="kusocolla02__face face-01">
+                @for($i =0; $i<$image->face_num; $i++)
+                    <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                @endfor
+            </div>
+            <div class="kusocolla02__face face-02">
+                @for($i =0; $i<$image->face_num; $i++)
+                    <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                @endfor
+            </div>
+            <div class="kusocolla02__face face-03">
+                @for($i =0; $i<$image->face_num; $i++)
+                    <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                @endfor
+            </div>
+            <div class="kusocolla02__face face-04">
+                @for($i =0; $i<$image->face_num; $i++)
+                    <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+                @endfor
             </div>
         </div>
     </div>
