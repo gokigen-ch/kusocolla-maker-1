ae8a060a61ca107a941831ba6cae936606c15d18
yamada <eri.genkidama@gmail.com>
2020-01-31 15:05:57 +0900

    クソコラ02の画像表示

--- resources/views/kusocolas/2.blade.php
+++ resources/views/kusocolas/2.blade.php
@@ -1,13 +1,11 @@
-    <div class="kusocolla">
-        <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-01.jpg" alt="">
-        <div class="kusocolla__img-move move01">
-            <div class="kusocolla__img__inner">
-                <div class="kusocolla__face face-04">
+    <div class="kusocolla02">
+        <div class="kusocolla02__img-move move01">
+            <div class="kusocolla02__img__inner">
+                <div class="kusocolla02__face face-01">
                     @for($i =0; $i<$image->face_num; $i++)
                         <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
                     @endfor
                 </div>
-                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-02.png" alt="">
             </div>
         </div>
     </div>
