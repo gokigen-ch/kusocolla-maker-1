1867c29c4beadbc06eb66ec46b0f35f806a6031e
yamada <eri.genkidama@gmail.com>
2020-02-05 01:28:53 +0900

    クソコラ01の画像を表示

--- resources/views/kusocolas/1.blade.php
+++ resources/views/kusocolas/1.blade.php
@@ -1,33 +1,38 @@
     <div class="kusocolla">
-        <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-01.jpg" alt="">
-        <div class="kusocolla__face face-01">
-            @for($i =0; $i<$image->face_num; $i++)
-                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-            @endfor
-        </div>
-        <div class="kusocolla__face face-02">
-            @for($i =0; $i<$image->face_num; $i++)
-                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-            @endfor
-        </div>
-        <div class="kusocolla__face face-03">
-            @for($i =0; $i<$image->face_num; $i++)
-                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-            @endfor
-        </div>
-        <div class="kusocolla__face face-05 move02">
-            @for($i =0; $i<$image->face_num; $i++)
-                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-            @endfor
-        </div>
+
         <div class="kusocolla__img-move move01">
             <div class="kusocolla__img__inner">
-                <div class="kusocolla__face face-04">
-                    @for($i =0; $i<$image->face_num; $i++)
-                        <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
-                    @endfor
-                </div>
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-01.jpg" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move02">
+            <div class="kusocolla__img__inner">
                 <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-02.png" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move03">
+            <div class="kusocolla__img__inner">
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-03.png" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move04">
+            <div class="kusocolla__img__inner">
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-04.png" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move05">
+            <div class="kusocolla__img__inner">
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-05.png" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move06">
+            <div class="kusocolla__img__inner">
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-06.png" alt="">
+            </div>
+        </div>
+        <div class="kusocolla__img-move move07">
+            <div class="kusocolla__img__inner">
+                <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-07.png" alt="">
             </div>
         </div>
     </div>
