15460c524a8d0883103c529f0720112b3c69af26
yamada <eri.genkidama@gmail.com>
2020-02-12 09:03:35 +0900

    クソコラ01に顔を追加

--- resources/views/kusocolas/1.blade.php
+++ resources/views/kusocolas/1.blade.php
@@ -12,7 +12,6 @@
 @endsection
 @section('kusocola')
     <div class="kusocolla">
-
         <div class="kusocolla__img-move move01">
             <div class="kusocolla__img__inner">
                 <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-01.jpg" alt="">
@@ -38,6 +37,12 @@
                 <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-05.png" alt="">
             </div>
         </div>
+        <div class="kusocolla__face face-01">
+            @for($i =0; $i<$image->face_num; $i++)
+                <img src="{{url("/")}}/storage/image/{{$image->getFolder()}}/{{$i}}.{{$image->getExtension()}}" alt="image" style="max-width:100%">
+            @endfor
+        </div>
+
         <div class="kusocolla__img-move move06">
             <div class="kusocolla__img__inner">
                 <img class="kusocolla__img-fixed" src="/images/kusocolla01/kusocolla01-06.png" alt="">
